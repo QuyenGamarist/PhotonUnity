87dfa0906870ce374e29556b7b169624d8d9f09b
ADMIN\Admin <kingahotdome@gmail.com>
2021-05-20 10:32:43 +0700

    1

---
+++ PhotonLobby/PhotonLobby/Assets/PUN2Guide/Scripts/ColorRace/CRController.cs
@@ -0,0 +1,83 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CRController : SingletonMono<CRController>
+{
+    public List<Color> ColorGround = new List<Color>();
+    public List<Color> ColorPlayer = new List<Color>();
+    public List<GameObject> Grounds;
+    public GameObject Ground = null;
+    public int Score;
+
+
+    float lastX = 0;
+    public enum GameState
+    {
+        Pause,
+        Playing,
+        GameOver,
+    }
+    public static GameState gamestate { get; set; } = GameState.Pause;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamestate = GameState.Playing;
+
+            StartSpawn();
+        
+        
+        AddColor(ColorPlayer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera.main.transform.Translate(new Vector3(1, 0, 0) * 3 * Time.deltaTime);
+
+    }
+    public void StartSpawn()
+    {
+        lastX = Camera.main.transform.position.x;
+        InvokeRepeating("Spawn", 0.0f, 1f);
+    }
+
+
+    private void Spawn()
+    {
+        AddColor(ColorGround);
+
+        if (gamestate == GameState.GameOver)
+        {
+            CancelInvoke("Spawn");
+        }
+        else transform.Translate(new Vector3(1, 0, 0) * 3.0f * Time.deltaTime);
+
+        if (Ground != null)
+        {
+            GameObject pip = (GameObject)PhotonNetwork.Instantiate("Ground", new Vector3(lastX + 5, 0.4f, 0), Quaternion.identity);
+            lastX = pip.transform.position.x;
+        }
+    }
+    public void AddColor(List<Color> mylist)
+    {
+        ColorGround.Clear();
+        mylist.Add(Color.red);
+        mylist.Add(Color.blue);
+        mylist.Add(Color.green);
+        mylist.Add(Color.yellow);
+    }
+    public void SetColor(GameObject Pipe, Color rdlist)
+    {
+        Pipe.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+        Pipe.GetComponent<Renderer>().material.SetColor("_EmissionColor", rdlist);
+    }
+
+    public void GameOver()
+    {
+        gamestate = GameState.GameOver;
+        Time.timeScale = 0;
+    }
+}
---
+++ PhotonLobby/PhotonLobby/Assets/PUN2Guide/Scripts/Singleton.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+    public class Singleton<T> where T : new()
+    {
+        private static T singleton = new T();
+
+        public static T instance
+        {
+            get
+            {
+                return singleton;
+            }
+        }
+    }
+    public class SingletonMono<T> : MonoBehaviour where T : UnityEngine.Component
+    {
+
+        private void Reset()
+        {
+            gameObject.name = typeof(T).ToString();
+        }
+
+        protected static T singleton;
+
+        public static T Instance
+        {
+            get
+            {
+                if (SingletonMono<T>.singleton == null)
+                {
+                    SingletonMono<T>.singleton = (T)FindObjectOfType(typeof(T));
+                    if (SingletonMono<T>.singleton == null)
+                    {
+                        GameObject go = new GameObject();
+                        go.name = typeof(T).ToString();
+                        SingletonMono<T>.singleton = go.AddComponent<T>();
+                    }
+                }
+                return SingletonMono<T>.singleton;
+            }
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (singleton == this)
+            {
+                singleton = null;
+            }
+        }
+    }
+
+    public class SingletonDontDestroy<T> : SingletonMono<T> where T : UnityEngine.Component
+    {
+        #region Methods
+
+        /// <summary>
+        /// Use this for initialization.
+        /// </summary>
+        protected virtual void Awake()
+        {
+            if (singleton == null)
+            {
+                singleton = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+        }
+
+        #endregion
+    }
