0fb9e77a1aece5521d41fb0e855d0d5f6c811240
QuangQuyen <kingahotdome@gmail.com>
2021-05-27 13:57:20 +0700

    comit 27/5

---
+++ PhotonLobby/Assets/PUN2Guide/Scripts/ColorRace/PunManager.cs
@@ -0,0 +1,51 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunManager : MonoBehaviour
+{
+    public List<int> SetIntRandom;
+    public List<int> RandomGround;
+    PhotonView photonView;
+
+    // Start is called before the first frame update
+  
+    void Start()
+    {
+        photonView = PhotonView.Get(this);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetrandomInt()
+    {
+        SetIntRandom.Clear();
+        SetIntRandom.Add(0);
+        SetIntRandom.Add(1);
+        SetIntRandom.Add(2);
+        SetIntRandom.Add(3);
+        
+    }
+
+    [PunRPC]
+    public List<int> SetColorGround()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            int j = Random.Range(0, SetIntRandom.Count);         
+            RandomGround.Add(SetIntRandom[j]);
+            SetIntRandom.RemoveAt(j);
+        }
+        return RandomGround;
+
+    }
+    public void CallRandomInt()
+    {
+        photonView.RPC("SetColorGround", RpcTarget.All, null);
+    }
+}
